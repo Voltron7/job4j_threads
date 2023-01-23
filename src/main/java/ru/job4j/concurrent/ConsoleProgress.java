2b441e12666e03118175f2a3de107d303708a6e4
Voltron7 <aquastar.minsk@gmail.com>
2023-01-23 18:23:19 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,30 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        char[] process = new char[] {'|', '/', '-', '\\'};
+        try {
+            while (!Thread.currentThread().isInterrupted()) {
+                for (int index = 0; index < process.length; index++) {
+                    Thread.sleep(500);
+                    System.out.print("\r load: " + process[index]);
+                }
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            Thread progress = new Thread(new ConsoleProgress());
+            progress.start();
+            Thread.sleep(5000);
+            progress.interrupt();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
