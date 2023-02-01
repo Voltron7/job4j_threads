f933b2f7fd0da52014fb79e406ddf61ee8f1b7f5
Voltron7 <aquastar.minsk@gmail.com>
2023-02-01 14:15:48 +0300

    2. JCIP. Настройка библиотеки [#268575 #386845]

--- src/test/java/ru/job4j/threads/jcip/CountTest.java
+++ src/test/java/ru/job4j/threads/jcip/CountTest.java
@@ -7,18 +7,13 @@ class CountTest {
 
     @Test
     public void whenExecute2ThreadThen2() throws InterruptedException {
-        /* Создаем счетчик. */
         final Count count = new Count();
-        /* Создаем нити. */
         Thread first = new Thread(count::increment);
         Thread second = new Thread(count::increment);
-        /* Запускаем нити. */
         first.start();
         second.start();
-        /* Заставляем главную нить дождаться выполнения наших нитей. */
         first.join();
         second.join();
-        /* Проверяем результат. */
         assertThat(count.get()).isEqualTo(2);
     }
 }
